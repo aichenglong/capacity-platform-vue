137ceabe00e0924a39eba222d214a616cf7a5b31
ICL <776172520@qq.com>
2018-06-25 20:49:35 +0800

    添加element-ui

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, { locale })
+Vue.use(router)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
